22c3757595a09887a9ebf17ee621265ac52fe56c
Daniel Cunningham <daniel.cunningham@gmx.net>
2022-06-25 07:57:22 +0200

    Create first two Pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.scss';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
 
 function App() {
   return (
     <>
     
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
     
     </>
   );
